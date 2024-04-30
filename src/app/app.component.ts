ebfbbe05474f669acf790e472672a2b4c0114afe
Rafael Bratfich <rbratfichdeo@outlook.com>
2024-04-30 12:35:22 -0500

    New Components and Routing.ts Created the header and footer components, also started implementing routing into the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BodyTwoComponent } from "./expense/body-two/body-two.component";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [BodyTwoComponent]
+    imports: [
+      BodyTwoComponent,
+      HeaderComponent,
+      FooterComponent,
+      RouterOutlet
+    ]
 })
 export class AppComponent {
   title = 'expense-tracker';
